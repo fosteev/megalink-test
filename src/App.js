ab93b83d64475d14a3061c859a58a716a7d76ede
Andrey <fosteev3@gmail.com>
2020-06-13 11:12:54 +0300

    Init webpack babel router

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react'
+import './App.css';
+
+export default function Root() {
+    return (<div>React + webpack 4 + react router 4 + babel 7</div>);
+}
---
+++ webpack.common.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    useBuiltIns: "usage",
+                                    "corejs": "2",
+                                    targets: {
+                                        browsers: "> 1%, not ie 11, not op_mini all"
+                                    }
+                                }
+                            ],
+                            "@babel/preset-react"
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-transform-runtime'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Production'
+        })
+    ],
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true,
+        hot: true,
+        port: 3000
+    }
+};
