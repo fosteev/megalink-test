1104e7ad1a1e76fe35e87058e9dce721bf6264bf
Andrey <fosteev3@gmail.com>
2020-06-13 11:35:44 +0300

    Add image rule

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,10 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            },
         ]
     },
     resolve: {
