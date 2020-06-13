ff1b5ad690f30829a3821d9c767604be0373bf39
Andrey <fosteev3@gmail.com>
2020-06-13 12:44:07 +0300

    Add main layout and AppBar

--- src/layouts/Main/components/AppBar/AppBar.js
+++ src/layouts/Main/components/AppBar/AppBar.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import './style.css';
+import Logo from '../../../../images/logo.svg';
+import {SearchInput, Avatar, Store} from "../index";
+
+const menuItems = [
+    'Home',
+    'Categories',
+    'Brands',
+    'Features'
+]
 
 export default function AppBar() {
     return (
-        <div>AppBar</div>
+        <header className={'AppBar'}>
+            <div className="AppBar__content">
+
+                <div className={'AppBar__left'}>
+                    <img src={Logo} className={'AppBar__logo'}/>
+
+                    <div className={"AppBar__menu"}>
+                        {
+                            menuItems.map(i => <div key={i}
+                                                    className="AppBar__item">{i}</div>)
+                        }
+                    </div>
+                </div>
+
+                <div className="AppBar__right">
+
+                    <SearchInput />
+
+                    <Avatar />
+
+                    <Store />
+
+                </div>
+
+            </div>
+
+        </header>
     )
 }
--- src/layouts/Main/components/index.js
+++ src/layouts/Main/components/index.js
@@ -1,2 +1,4 @@
 export {default as AppBar} from './AppBar';
 export {default as SearchInput} from './SearchInput';
+export {default as Avatar} from './Avatar';
+export {default as Store} from './Store';
