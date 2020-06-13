4e10a90b78d7cab4c17454cb7c6d3d1746f0ec63
Andrey <fosteev3@gmail.com>
2020-06-13 12:20:00 +0300

    add Appbar component avatar

---
+++ src/layouts/Main/components/Avatar/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import AvatarImage from '../../../../images/avatar.png'
+
+export default function Avatar() {
+    return (
+        <div className={'Avatar'} >
+            <div className="Avatar__logo" style={{
+                'background-image': `url(${AvatarImage})`
+            }} />
+        </div>
+    )
+}
