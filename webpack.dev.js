07a329f814ee2653c5570ff4bed9f3a4af79f700
Andrey <fosteev3@gmail.com>
2020-06-13 13:26:46 +0300

    webpack dev plugins active

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const WebpackBar = require('webpackbar');
+const SizePlugin = require('size-plugin');
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
+    plugins: [
+        new ErrorOverlayPlugin(),
+        new WebpackBar(),
+        new SizePlugin()
+    ],
     devServer: {
         contentBase: './dist'
     }
